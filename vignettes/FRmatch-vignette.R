2e8ae9c5f9e263f4f9ea1e4fc8da8a2612d3d25a
zhangy <zhangy@jcvi.org>
2021-05-06 22:31:49 -0700

    updated vignette

--- vignettes/FRmatch-vignette.R
+++ vignettes/FRmatch-vignette.R
@@ -2,7 +2,7 @@
 knitr::opts_chunk$set(
   catch = TRUE,
   collapse = TRUE,
-  comment = "##"
+  comment = "#>"
 )
 
 ## ---- install, eval=FALSE-----------------------------------------------------
@@ -14,46 +14,76 @@ library(FRmatch)
 library(SingleCellExperiment)
 library(dplyr)
 library(tibble)
+library(data.table)
 
 ## ---- shinyapp, eval=FALSE----------------------------------------------------
 #  runShiny()
 
-## ---- data, warning=FALSE, message=FALSE--------------------------------------
+## ---- data-example, warning=FALSE, message=FALSE------------------------------
 data(sce.example)
 sce.example
 
-## ---- cluster-size, , warning=FALSE, message=FALSE----------------------------
-knitr::kable(table(colData(sce.example)$cluster_membership), col.names=c("Cluster", "Size"), row.names=1:15)
+## ---- data-layer1-------------------------------------------------------------
+## rename the data object
+sce.layer1 <- sce.example
+## cell type clusters and cluster sizes
+knitr::kable(table(colData(sce.layer1)$cluster_membership), col.names=c("Cluster", "Size"), row.names=1:15)
 
-## ---- data-split, warning=FALSE, message=FALSE--------------------------------
-set.seed(999)
-all <- colData(sce.example) %>% as.data.frame() %>% rownames_to_column()
-sam1 <- all %>% group_by(cluster_membership) %>% sample_frac(.5)
-sam2 <- dplyr::setdiff(all, sam1)
+## ---- data-read-aws-----------------------------------------------------------
+## read in pieces of input data - this may take a few minutes
+cell_by_gene_expression <- fread("https://allen-data.s3-us-west-2.amazonaws.com/cell_by_gene_expression.csv")
+cell_cluster_labels <- fread("https://allen-data.s3-us-west-2.amazonaws.com/cell_cluster_labels.csv")
+NSForest_marker_genes <- fread("https://allen-data.s3-us-west-2.amazonaws.com/NSForest_marker_genes.csv")
+NSForest_fscores <- fread("https://allen-data.s3-us-west-2.amazonaws.com/NSForest_fscores.csv")
+MTG_taxonomy <- fread("https://allen-data.s3-us-west-2.amazonaws.com/MTG_taxonomy.csv")$x #need to be a vector
 
-sce.sam1 <- sce.example[,sam1$rowname]
-sce.sam2 <- sce.example[,sam2$rowname]
+## unique markers
+unique_markers <- unique(NSForest_marker_genes$markerGene)
 
-## ---- clusterSize, fig.height = 9, fig.width = 7------------------------------
-plot_clusterSize(sce.sam1, sce.sam2)
+## ---- make-data-object--------------------------------------------------------
+sce.MTG <- make_data_object(dat = cell_by_gene_expression,
+                            tab = cell_cluster_labels,
+                            markers = unique_markers,
+                            ## below are optional input data
+                            cluster_marker_info = NSForest_marker_genes,
+                            f_score = NSForest_fscores,
+                            cluster_order = MTG_taxonomy)
+
+## ---- data-MTG----------------------------------------------------------------
+sce.MTG
 
 ## ---- barcode-plot, fig.height = 5, fig.width = 7-----------------------------
-plot_cluster_by_markers(sce.sam1, cluster.name = "i1_i90_COL5A2_Ndnf_Car4")
+plot_cluster_by_markers(sce.example, cluster.name = "e1_e299_SLC17A7_L5b_Cdh13", name.E1 = "Layer1_")
+plot_cluster_by_markers(sce.example, cluster.name = "g1_g48_GLI3_Astro_Gja1", name.E1 = "Layer1_")
+plot_cluster_by_markers(sce.example, cluster.name = "i1_i90_COL5A2_Ndnf_Car4", name.E1 = "Layer1_")
 
-## ---- FRmatch12, warning=FALSE, message=FALSE---------------------------------
-rst12 <- FRmatch(sce.query = sce.sam1, sce.ref = sce.sam2)
+## ---- plot-clusterSize, fig.height = 9, fig.width = 9-------------------------
+plot_clusterSize(sce.layer1, sce.MTG, name.E1 = "Layer1", name.E2 = "MTG")
 
-## ---- FRmatch21, warning=FALSE, message=FALSE---------------------------------
-rst21 <- FRmatch(sce.query = sce.sam2, sce.ref = sce.sam1)
+## ---- FRmatch_layer1toMTG, warning=FALSE, message=FALSE-----------------------
+rst.layer1toMTG <- FRmatch(sce.query = sce.layer1, sce.ref = sce.MTG)
 
-## ---- FRmatch-bi-plot, fig.height = 5, fig.width = 7--------------------------
-plot_bi_FRmatch(rst12, rst21, reorder = FALSE)
+## ---- FRmatch_MTGtolayer1, warning=FALSE, message=FALSE-----------------------
+rst.MTGtolayer1 <- FRmatch(sce.query = sce.MTG, sce.ref = sce.layer1)
 
-## ---- FRmatch-plot-matches, fig.height = 5, fig.width = 7---------------------
-plot_FRmatch(rst12, reorder = FALSE)
+## ---- FRmatch-bi-plot, fig.height = 13.5, fig.width = 7-----------------------
+plot_bi_FRmatch(rst.layer1toMTG, rst.MTGtolayer1, name.E1="Layer1_", name.E2="MTG_")
+
+## ---- FRmatch-plot-matches, fig.height = 13.5, fig.width = 7------------------
+## to visualize the one-directional matches
+plot_FRmatch(rst.layer1toMTG)
 
 ## ---- FRmatch-plot-padj, fig.height = 4, fig.width = 7------------------------
-plot_FRmatch(rst12, type = "padj", reorder = FALSE)
+## to visualize the adjusted p-values for each query cluster
+plot_FRmatch(rst.layer1toMTG, type = "padj")
+
+## ---- FRmatch-plot-matches-2, fig.height = 5, fig.width = 13.5----------------
+## to visualize the one-directional matches
+plot_FRmatch(rst.MTGtolayer1)
+
+## ---- FRmatch-plot-padj-2, fig.height = 4, fig.width = 8----------------------
+## to visualize the adjusted p-values for each query cluster
+plot_FRmatch(rst.MTGtolayer1, type = "padj")
 
 ## ---- FR-test, fig.height = 7, fig.width = 7----------------------------------
 # simulate some synthetic data from the same distribution
