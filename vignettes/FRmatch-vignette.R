f762b967175d27cfc6c7720c2cda6db64e0242e9
Yun Zhang <zhangy@jcvi.org>
2022-11-22 12:22:25 -0800

    see vignette in tutorial link

--- vignettes/FRmatch-vignette.R
+++ vignettes/FRmatch-vignette.R
@@ -16,9 +16,6 @@ library(dplyr)
 library(tibble)
 library(data.table)
 
-## ---- shinyapp, eval=FALSE----------------------------------------------------
-#  runShiny()
-
 ## ---- data-example, warning=FALSE, message=FALSE------------------------------
 data(sce.example)
 sce.example
@@ -27,15 +24,15 @@ sce.example
 ## rename the data object
 sce.layer1 <- sce.example
 ## cell type clusters and cluster sizes
-knitr::kable(table(colData(sce.layer1)$cluster_membership), col.names=c("Cluster", "Size"), row.names=1:15)
+knitr::kable(table(colData(sce.layer1)$cluster_membership), col.names=c("Cluster", "Size"))
 
-## ---- data-read-aws-----------------------------------------------------------
+## ---- data-read---------------------------------------------------------------
 ## read in pieces of input data - this may take a few minutes
-cell_by_gene_expression <- fread("https://allen-data.s3-us-west-2.amazonaws.com/cell_by_gene_expression.csv")
-cell_cluster_labels <- fread("https://allen-data.s3-us-west-2.amazonaws.com/cell_cluster_labels.csv")
-NSForest_marker_genes <- fread("https://allen-data.s3-us-west-2.amazonaws.com/NSForest_marker_genes.csv")
-NSForest_fscores <- fread("https://allen-data.s3-us-west-2.amazonaws.com/NSForest_fscores.csv")
-MTG_taxonomy <- fread("https://allen-data.s3-us-west-2.amazonaws.com/MTG_taxonomy.csv")$x #need to be a vector
+cell_by_gene_expression <- fread("cell_by_gene_expression.csv")
+cell_cluster_labels <- fread("cell_cluster_labels.csv")
+NSForest_marker_genes <- fread("NSForest_marker_genes.csv")
+NSForest_fscores <- fread("NSForest_fscores.csv")
+MTG_taxonomy <- fread("MTG_taxonomy.csv")$x #need to be a vector
 
 ## unique markers
 unique_markers <- unique(NSForest_marker_genes$markerGene)
@@ -53,18 +50,18 @@ sce.MTG <- make_data_object(dat = cell_by_gene_expression,
 sce.MTG
 
 ## ---- barcode-plot, fig.height = 5, fig.width = 7-----------------------------
-plot_cluster_by_markers(sce.example, cluster.name = "e1_e299_SLC17A7_L5b_Cdh13", name.E1 = "Layer1_")
-plot_cluster_by_markers(sce.example, cluster.name = "g1_g48_GLI3_Astro_Gja1", name.E1 = "Layer1_")
-plot_cluster_by_markers(sce.example, cluster.name = "i1_i90_COL5A2_Ndnf_Car4", name.E1 = "Layer1_")
+plot_cluster_by_markers(sce.example, cluster.name = "e1_e299_SLC17A7_L5b_Cdh13", name.self = "Layer1_")
+plot_cluster_by_markers(sce.example, cluster.name = "g1_g48_GLI3_Astro_Gja1", name.self = "Layer1_")
+plot_cluster_by_markers(sce.example, cluster.name = "i1_i90_COL5A2_Ndnf_Car4", name.self = "Layer1_")
 
 ## ---- plot-clusterSize, fig.height = 9, fig.width = 9-------------------------
 plot_clusterSize(sce.layer1, sce.MTG, name.E1 = "Layer1", name.E2 = "MTG")
 
 ## ---- FRmatch_layer1toMTG, warning=FALSE, message=FALSE-----------------------
-rst.layer1toMTG <- FRmatch(sce.query = sce.layer1, sce.ref = sce.MTG)
+rst.layer1toMTG <- FRmatch(sce.query = sce.layer1, sce.ref = sce.MTG, subsamp.size = 10)
 
 ## ---- FRmatch_MTGtolayer1, warning=FALSE, message=FALSE-----------------------
-rst.MTGtolayer1 <- FRmatch(sce.query = sce.MTG, sce.ref = sce.layer1)
+rst.MTGtolayer1 <- FRmatch(sce.query = sce.MTG, sce.ref = sce.layer1, subsamp.size = 10)
 
 ## ---- FRmatch-bi-plot, fig.height = 13.5, fig.width = 7-----------------------
 plot_bi_FRmatch(rst.layer1toMTG, rst.MTGtolayer1, name.E1="Layer1_", name.E2="MTG_")
